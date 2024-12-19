8557b8124d9be876955431debbc92a883edfd6c4
SnoxE <oicmyzs2000@gmail.com>
2024-12-19 11:14:00 +0100

    Initialize project and repository

---
+++ src/main/java/allegro/agh/auto_detailing/AutoDetailingApplication.java
@@ -0,0 +1,13 @@
+package allegro.agh.auto_detailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDetailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDetailingApplication.class, args);
+	}
+
+}
