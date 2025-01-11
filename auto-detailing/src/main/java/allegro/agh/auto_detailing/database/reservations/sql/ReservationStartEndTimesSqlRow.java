6ec929ab71110947b19e65cabae26995a3de5117
[xShadeen] <[adrian@slawinski.pl]>
2025-01-11 13:17:31 +0100

    Add logic for reservations

---
+++ auto-detailing/src/main/java/allegro/agh/auto_detailing/database/reservations/sql/ReservationStartEndTimesSqlRow.java
@@ -0,0 +1,10 @@
+package allegro.agh.auto_detailing.database.reservations.sql;
+
+import java.time.LocalDateTime;
+
+public record ReservationStartEndTimesSqlRow(
+        LocalDateTime startAt,
+        LocalDateTime endAt) {
+    public static final String START_AT = "start_at";
+    public static final String END_AT = "end_at";
+}
---
+++ auto-detailing/src/main/java/allegro/agh/auto_detailing/database/user/dto/PasswordDto.java
@@ -0,0 +1,8 @@
+package allegro.agh.auto_detailing.database.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record PasswordDto(
+        @JsonProperty("old_password") String oldPassword,
+        @JsonProperty("new_password") String newPassword) {}
+
