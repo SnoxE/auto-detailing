ad660a7e5936798d4346471eb09e058c8c574e3e
[xShadeen] <[adrian@slawinski.pl]>
2025-01-27 15:17:53 +0100

    Fix

--- auto-detailing/src/test/java/allegro/agh/auto_detailing/ServiceControllerTest.java
+++ auto-detailing/src/test/java/allegro/agh/auto_detailing/ServiceControllerTest.java
@@ -1,10 +1,15 @@
 package allegro.agh.auto_detailing;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import allegro.agh.auto_detailing.common.dto.ContentDto;
 import allegro.agh.auto_detailing.controller.ServicesController;
 import allegro.agh.auto_detailing.database.services.ServiceDto;
 import allegro.agh.auto_detailing.database.services.ServiceNamesDto;
 import allegro.agh.auto_detailing.service.ServicesService;
+import java.sql.Time;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,7 +33,11 @@ class ServicesControllerTest {
 
     assertNotNull(result);
     assertEquals(1, result.content().size());
+<<<<<<< HEAD
     assertEquals("Cleaning", result.content().get(0).name());
+=======
+    assertEquals("Cleaning", result.content().get(0).getName());
+>>>>>>> fffabcd (Fix)
 
     verify(servicesService, times(1)).getServiceNames();
   }
@@ -45,7 +54,11 @@ class ServicesControllerTest {
 
     assertNotNull(result);
     assertEquals(1, result.size());
+<<<<<<< HEAD
     assertEquals("Cleaning", result.get(0).name());
+=======
+    assertEquals("Cleaning", result.get(0).getServiceName());
+>>>>>>> fffabcd (Fix)
     verify(servicesService, times(1)).getServiceByNameAndCarSize("Cleaning", "Small");
   }
 }
