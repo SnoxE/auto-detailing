3504d2996e2f897d0d17e25cf49ee263f6f25006
[xShadeen] <[adrian@slawinski.pl]>
2025-01-27 15:10:00 +0100

    Tests for auto-detailing added

--- auto-detailing/src/main/java/allegro/agh/auto_detailing/database/services/ServiceNamesDto.java
+++ auto-detailing/src/main/java/allegro/agh/auto_detailing/database/services/ServiceNamesDto.java
@@ -2,4 +2,8 @@ package allegro.agh.auto_detailing.database.services;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record ServiceNamesDto(@JsonProperty("name") String name) {}
+public record ServiceNamesDto(@JsonProperty("name") String name) {
+    public String getName() {
+        return this.name;
+    }
+}
