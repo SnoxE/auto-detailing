42f8ec81d7343b6f4254251a8a7176b5c461da2e
[xShadeen] <[adrian@slawinski.pl]>
2025-01-27 15:10:00 +0100

    Tests for auto-detailing added

--- auto-detailing/src/test/java/allegro/agh/auto_detailing/CarServiceTest.java
+++ auto-detailing/src/test/java/allegro/agh/auto_detailing/CarServiceTest.java
@@ -1,20 +1,28 @@
 package allegro.agh.auto_detailing;
 
+<<<<<<< HEAD
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+=======
+>>>>>>> 3504d29 (Tests for auto-detailing added)
 import allegro.agh.auto_detailing.common.dto.ContentDto;
 import allegro.agh.auto_detailing.database.car.CarDto;
 import allegro.agh.auto_detailing.database.car.sql.CarSqlRow;
 import allegro.agh.auto_detailing.database.car.sql.CarSqlService;
 import allegro.agh.auto_detailing.service.CarService;
+<<<<<<< HEAD
 import java.util.List;
+=======
+import org.junit.jupiter.api.BeforeEach;
+>>>>>>> 3504d29 (Tests for auto-detailing added)
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+<<<<<<< HEAD
 @ExtendWith(MockitoExtension.class)
 class CarServiceTest {
 
@@ -47,4 +55,45 @@ class CarServiceTest {
     assertEquals("Toyota", result.content().get(0).make());
     verify(carSqlService, times(1)).getCars("1");
   }
+=======
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CarServiceTest {
+
+    @Mock
+    private CarSqlService carSqlService;
+
+    @InjectMocks
+    private CarService carService;
+
+    @Test
+    void shouldAddCar() {
+        carService.addCar(1, "Toyota", "Corolla", "2020", "M", "Black");
+
+        verify(carSqlService, times(1)).createCar(1, "Toyota", "Corolla", "2020", "M", "Black");
+    }
+
+    @Test
+    void shouldDeleteCar() {
+        carService.deleteCar(1, 2);
+
+        verify(carSqlService, times(1)).deleteCar(1, 2);
+    }
+
+    @Test
+    void shouldReturnUserCars() {
+        List<CarSqlRow> mockRows = List.of(new CarSqlRow(1, "Toyota", "Corolla", "2020", "M", "Black"));
+        when(carSqlService.getCars("1")).thenReturn(mockRows);
+
+        ContentDto<CarDto> result = carService.getUserCars("1");
+
+        assertEquals(1, result.content().size());
+        assertEquals("Toyota", result.content().get(0).make());
+        verify(carSqlService, times(1)).getCars("1");
+    }
+>>>>>>> 3504d29 (Tests for auto-detailing added)
 }
--- auto-detailing/src/test/java/allegro/agh/auto_detailing/ServiceControllerTest.java
+++ auto-detailing/src/test/java/allegro/agh/auto_detailing/ServiceControllerTest.java
@@ -1,21 +1,28 @@
 package allegro.agh.auto_detailing;
 
+<<<<<<< HEAD
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+=======
+>>>>>>> 3504d29 (Tests for auto-detailing added)
 import allegro.agh.auto_detailing.common.dto.ContentDto;
 import allegro.agh.auto_detailing.controller.ServicesController;
 import allegro.agh.auto_detailing.database.services.ServiceDto;
 import allegro.agh.auto_detailing.database.services.ServiceNamesDto;
 import allegro.agh.auto_detailing.service.ServicesService;
+<<<<<<< HEAD
 import java.sql.Time;
 import java.util.List;
+=======
+>>>>>>> 3504d29 (Tests for auto-detailing added)
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+<<<<<<< HEAD
 @ExtendWith(MockitoExtension.class)
 class ServicesControllerTest {
 
@@ -69,4 +76,52 @@ class ServicesControllerTest {
 >>>>>>> 021646f (Deleting getters from ServceDto & ServiceNamesDto)
     verify(servicesService, times(1)).getServiceByNameAndCarSize("Cleaning", "Small");
   }
+=======
+import java.sql.Time;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ServicesControllerTest {
+
+    @Mock
+    private ServicesService servicesService;
+
+    @InjectMocks
+    private ServicesController servicesController;
+
+    @Test
+    void shouldReturnServiceNames() {
+        ServiceNamesDto cleaningService = new ServiceNamesDto("Cleaning");
+
+        when(servicesService.getServiceNames()).thenReturn(new ContentDto<>(List.of(cleaningService)));
+
+        ContentDto<ServiceNamesDto> result = servicesController.getServiceNames();
+
+        assertNotNull(result);
+        assertEquals(1, result.content().size());
+        assertEquals("Cleaning", result.content().get(0).getName());
+
+        verify(servicesService, times(1)).getServiceNames();
+    }
+
+
+    @Test
+    void shouldReturnServiceByNameAndCarSize() {
+        ServiceDto mockService = new ServiceDto(1, "Cleaning", 25, Time.valueOf("01:30:00"), "M");
+
+        List<ServiceDto> mockResponse = List.of(mockService);
+
+        when(servicesService.getServiceByNameAndCarSize("Cleaning", "Small")).thenReturn(mockResponse);
+
+        List<ServiceDto> result = servicesController.getServiceId("Cleaning", "Small");
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("Cleaning", result.get(0).getServiceName());
+        verify(servicesService, times(1)).getServiceByNameAndCarSize("Cleaning", "Small");
+    }
+>>>>>>> 3504d29 (Tests for auto-detailing added)
 }
