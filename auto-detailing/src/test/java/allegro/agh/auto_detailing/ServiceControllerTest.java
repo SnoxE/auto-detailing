fffabcd6dfb4befd4dc7fab631122259048a26a5
[xShadeen] <[adrian@slawinski.pl]>
2025-01-27 15:17:53 +0100

    Fix

--- auto-detailing/src/test/java/allegro/agh/auto_detailing/ServiceControllerTest.java
+++ auto-detailing/src/test/java/allegro/agh/auto_detailing/ServiceControllerTest.java
@@ -1,60 +1,56 @@
 package allegro.agh.auto_detailing;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import allegro.agh.auto_detailing.common.dto.ContentDto;
 import allegro.agh.auto_detailing.controller.ServicesController;
 import allegro.agh.auto_detailing.database.services.ServiceDto;
 import allegro.agh.auto_detailing.database.services.ServiceNamesDto;
 import allegro.agh.auto_detailing.service.ServicesService;
+import java.sql.Time;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.sql.Time;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class ServicesControllerTest {
 
-    @Mock
-    private ServicesService servicesService;
+  @Mock private ServicesService servicesService;
 
-    @InjectMocks
-    private ServicesController servicesController;
+  @InjectMocks private ServicesController servicesController;
 
-    @Test
-    void shouldReturnServiceNames() {
-        ServiceNamesDto cleaningService = new ServiceNamesDto("Cleaning");
+  @Test
+  void shouldReturnServiceNames() {
+    ServiceNamesDto cleaningService = new ServiceNamesDto("Cleaning");
 
-        when(servicesService.getServiceNames()).thenReturn(new ContentDto<>(List.of(cleaningService)));
+    when(servicesService.getServiceNames()).thenReturn(new ContentDto<>(List.of(cleaningService)));
 
-        ContentDto<ServiceNamesDto> result = servicesController.getServiceNames();
+    ContentDto<ServiceNamesDto> result = servicesController.getServiceNames();
 
-        assertNotNull(result);
-        assertEquals(1, result.content().size());
-        assertEquals("Cleaning", result.content().get(0).getName());
+    assertNotNull(result);
+    assertEquals(1, result.content().size());
+    assertEquals("Cleaning", result.content().get(0).getName());
 
-        verify(servicesService, times(1)).getServiceNames();
-    }
+    verify(servicesService, times(1)).getServiceNames();
+  }
 
+  @Test
+  void shouldReturnServiceByNameAndCarSize() {
+    ServiceDto mockService = new ServiceDto(1, "Cleaning", 25, Time.valueOf("01:30:00"), "M");
 
-    @Test
-    void shouldReturnServiceByNameAndCarSize() {
-        ServiceDto mockService = new ServiceDto(1, "Cleaning", 25, Time.valueOf("01:30:00"), "M");
+    List<ServiceDto> mockResponse = List.of(mockService);
 
-        List<ServiceDto> mockResponse = List.of(mockService);
+    when(servicesService.getServiceByNameAndCarSize("Cleaning", "Small")).thenReturn(mockResponse);
 
-        when(servicesService.getServiceByNameAndCarSize("Cleaning", "Small")).thenReturn(mockResponse);
+    List<ServiceDto> result = servicesController.getServiceId("Cleaning", "Small");
 
-        List<ServiceDto> result = servicesController.getServiceId("Cleaning", "Small");
-
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        assertEquals("Cleaning", result.get(0).getServiceName());
-        verify(servicesService, times(1)).getServiceByNameAndCarSize("Cleaning", "Small");
-    }
+    assertNotNull(result);
+    assertEquals(1, result.size());
+    assertEquals("Cleaning", result.get(0).getServiceName());
+    verify(servicesService, times(1)).getServiceByNameAndCarSize("Cleaning", "Small");
+  }
 }
