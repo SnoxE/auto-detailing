26a5b55e29485356fde08544855175b3ae2d4fb5
Szymon Moździerz <95888909+SnoxE@users.noreply.github.com>
2025-01-28 23:49:17 +0100

    #9 Add unit tests using jUnit (#9)

---
+++ auto-detailing/src/test/java/allegro/agh/auto_detailing/CarServiceTest.java
@@ -0,0 +1,47 @@
+package allegro.agh.auto_detailing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import allegro.agh.auto_detailing.common.dto.ContentDto;
+import allegro.agh.auto_detailing.database.car.CarDto;
+import allegro.agh.auto_detailing.database.car.sql.CarSqlRow;
+import allegro.agh.auto_detailing.database.car.sql.CarSqlService;
+import allegro.agh.auto_detailing.service.CarService;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CarServiceTest {
+
+  @Mock private CarSqlService carSqlService;
+  @InjectMocks private CarService carService;
+
+  @Test
+  void shouldAddCar() {
+    carService.addCar(1, "Toyota", "Corolla", "2020", "M", "Black");
+    verify(carSqlService, times(1)).createCar(1, "Toyota", "Corolla", "2020", "M", "Black");
+  }
+
+  @Test
+  void shouldDeleteCar() {
+    carService.deleteCar(1, 2);
+    verify(carSqlService, times(1)).deleteCar(1, 2);
+  }
+
+  @Test
+  void shouldReturnUserCars() {
+    List<CarSqlRow> mockRows = List.of(new CarSqlRow(1, "Toyota", "Corolla", "2020", "M", "Black"));
+    when(carSqlService.getCars("1")).thenReturn(mockRows);
+    ContentDto<CarDto> result = carService.getUserCars("1");
+    assertEquals(1, result.content().size());
+    assertEquals("Toyota", result.content().getFirst().make());
+    verify(carSqlService, times(1)).getCars("1");
+  }
+}
